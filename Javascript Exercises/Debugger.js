b085d2e2c3d3906cd12d084ecb44cd6ece5a2769
AlexanderRN <AlexanderRNielsen@hotmail.dk>
2016-02-15 18:20:14 +0100

    Added to the exercises.

---
+++ Javascript Exercises/Debugger.js
@@ -0,0 +1,7 @@
+var a = {};
+var b = [];
+var c = true;
+var d = "dfg";
+var e = 123;
+var f = new Date();
+console.log("Add a breaking point on me");
--- Javascript Exercises/Hoisting.js
+++ Javascript Exercises/Hoisting.js
@@ -81,11 +81,103 @@ console.log("Increment: " + counter.increment());
 console.log("Increment: " + counter.increment());
 
 // Objects
-var person = {name:"Alexander", age:21, hobby:"Programming", email:"cph-an130@cphbusiness.dk"}
-console.log("\nInfo about: " + person.name);
+var myPerson = {name:"Alexander", age:21, hobby:"Programming", email:"cph-an130@cphbusiness.dk"}
+console.log("\nInfo about: " + myPerson.name);
 
-for(var property in person) {
-    console.log(person[property]);
+for(var property in myPerson) {
+    console.log(property + ": " + myPerson[property]);
 }
 
-person
+delete myPerson.email;
+
+console.log("\nInfo about: " + myPerson.name + ", without email.");
+for(var property in myPerson) {
+    console.log(property + ": " + myPerson[property]);
+}
+
+console.log("\n" + "Hobby? " + myPerson.hasOwnProperty("hobby"));
+console.log("\n" + "Email? " + myPerson.hasOwnProperty("email"));
+
+function person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.getPerson = function () {
+        return firstName + " " + lastName + ", " + age;
+    };
+}
+
+var testPerson = new person("Alexander", "Nielsen", 21);
+console.log("\n" + testPerson.getPerson());
+
+// Part 3 //
+
+var personX = {firstName:"Bo", lastName:"Knudsen", age:21};
+
+function listAllProperties(o) {
+    var objectToInspect;
+    var result = [];
+
+    for (objectToInspect = o; objectToInspect !== null; objectToInspect = Object.getPrototypeOf(objectToInspect)){
+        result = result.concat(Object.getOwnPropertyNames(objectToInspect));
+    }
+
+    return result;
+}
+
+console.log("\n" + listAllProperties(personX));
+delete personX.age;
+console.log("\n" + listAllProperties(personX));
+
+// Reusable Modules with Closures.
+var makeCounter = function () {
+    var privateCounter = 0;
+    function changeBy(val) {
+        privateCounter += val;
+    }
+    return {
+        increment: function() {changeBy(1);},
+        decrement: function() {changeBy(-1);},
+        value: function() {return privateCounter;},
+    }
+};
+
+var counter1 = makeCounter();
+console.log("\n" + counter1.value());
+counter1.increment();
+counter1.increment();
+console.log(counter1.value());
+
+var makePerson = function(fname, lname, personAge){
+    var firstName = fname;
+    var lastName = lname;
+    var age = personAge;
+    function changeFName(fname){
+        firstName = fname;
+    }
+    function changeLName(lname){
+        lastName = lname;
+    }
+    function changeAge(newAge){
+        age = newAge;
+    }
+    function getPerson() {
+        return firstName + " " + lastName + ", " + age;
+    }
+    return {
+        changeFirstName: function(newName) {changeFName(newName)},
+        changeLastName: function(newLastName) {changeLName(newLastName)},
+        changeAge: function(newAge) {changeAge(newAge)},
+        getInfo: function() { return getPerson();}
+
+    }
+};
+
+var person1 = makePerson("Alex", "Nielsen", 21);
+console.log(person1.getInfo());
+person1.changeFirstName("Bo");
+person1.changeLastName("Bullersen");
+person1.changeAge(26);
+console.log(person1.getInfo());
+
+console.log();
